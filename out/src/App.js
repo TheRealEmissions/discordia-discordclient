61283c3dd36d8dc499b74d781a63ff247dd943f4
therealemissions <me@liammartin.site>
2023-01-01 18:33:35 +0000

    updated

--- out/src/App.js
+++ out/src/App.js
@@ -1,5 +1,6 @@
 import { AuthConfig } from "../config/Auth.js";
 import BaseApp from "./BaseApp.js";
+import FS from "fs-extra-promise";
 class App extends BaseApp {
     events = [];
     constructor() {
@@ -7,7 +8,7 @@ class App extends BaseApp {
     }
     async init() {
         BaseApp.Events.getEventEmitter().emit(BaseApp.Events.GeneralEvents.INFO, "Discord Client Loaded");
-        this.importEvents();
+        await this.importEvents();
         try {
             await this.login();
         }
@@ -16,8 +17,17 @@ class App extends BaseApp {
         }
     }
     async importEvents() {
-        const readyEvent = await import("./events/Ready.js");
-        this.events.push(new readyEvent.default());
+        const files = await FS.readdirAsync("./src/events");
+        for (const file of files) {
+            if (!file.endsWith(".js"))
+                continue;
+            const name = file.split(".")[0];
+            await this.pushEvent(name);
+        }
+    }
+    async pushEvent(name) {
+        const event = await import(`./events/${name}.js`);
+        this.events.push(new event.default());
     }
     async login() {
         await this.getClient().login(AuthConfig.token);
